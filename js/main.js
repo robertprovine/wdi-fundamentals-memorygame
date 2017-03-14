31470f2d64bcc1a3bc6c52c1c6ce999450c293dc
Robert Provine <robertprovine@gmail.com>
2017-03-13 21:50:00 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -31,15 +31,28 @@ var checkForMatch = function () {
   }
 }
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+  var cardId = this.getAttribute("data-id");
   console.log("User flipped " + cards[cardId].rank);
   console.log("Suit: " + cards[cardId].suit);
   console.log("Card Image: " + cards[cardId].cardImage);
   cardsInPlay.push(cards[cardId].rank);
+  this.setAttribute("src", cards[cardId].cardImage);
   if (cardsInPlay.length === 2) {
     checkForMatch();
   }
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+  var cardElement;
+  var gameBoard = document.getElementById("game-board");
+  for (var i = 0; i < cards.length; i++) {
+    cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.png")
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
+    gameBoard.appendChild(cardElement);
+  }
+}
+
+createBoard();
